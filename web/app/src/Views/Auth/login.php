03a72c5590c80c32639319967d7f53f30ffa95ea
Yevhen <boycojoz@gmail.com>
2021-08-10 09:13:53 +0300

    add template

--- web/app/src/Views/Auth/login.php
+++ web/app/src/Views/Auth/login.php
@@ -1,21 +1,25 @@
 <?php
 include __DIR__ . "/../Layouts/header.php";
-include __DIR__ . "/../Layouts/navbar.php";
+include __DIR__ . "/../Layouts/navbarAuth.php";
 ?>
-    <form action="<?= url(); ?>" method="post">
-        <input type="hidden" name="csrf_token" value="<?= csrf_token(); ?>">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required>
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Войти</button>
-        <a href="/register" class="btn btn-danger">Зарегестрироваться</a>
-    </form>
+    <div class="container">
+        <form action="<?= url('registerStore'); ?>" method="post">
+            <input type="hidden" name="csrf_token" value="<?= csrf_token(); ?>">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required>
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
+            </div>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary mx-1">Войти</button>
+                <a href="<?= url('register'); ?>" class="btn btn-danger mx-1">Зарегестрироваться</a>
+            </div>
+        </form>
+    </div>
 <?php
 include __DIR__ . "/../Layouts/footer.php";
 ?>
--- web/app/src/Views/Auth/register.php
+++ web/app/src/Views/Auth/register.php
@@ -1,31 +1,33 @@
 <?php
 include __DIR__ . "/../Layouts/header.php";
-include __DIR__ . "/../Layouts/navbar.php";
+include __DIR__ . "/../Layouts/navbarAuth.php";
 ?>
-    <form action="<?= url('registerStore'); ?>" method="post">
-        <input type="hidden" name="csrf_token" value="<?= csrf_token(); ?>">
-        <div class="mb-3">
-            <label for="reg_name" class="form-label">Name</label>
-            <input type="name" minlength="3" class="form-control" id="reg_name" aria-describedby="emailName" name="name" required>
-        </div>
-        <div class="mb-3">
-            <label for="reg_email" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="reg_email" aria-describedby="emailHelp" name="email" required>
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="reg_password" class="form-label">Password</label>
-            <input type="password" minlength="6" class="form-control" id="reg_password" name="password" required>
-        </div>
-        <div class="mb-3">
-            <label for="reg_password_conf" class="form-label">Password Confirm</label>
-            <input type="password" minlength="6" class="form-control" id="reg_password_conf" name="password_conf" required>
-        </div>
-        <div class="d-flex justify-content-center">
-            <button type="submit" class="btn btn-primary mx-1">Зарегистрироваться</button>
-            <a href="/login" class="btn btn-danger mx-1">Назад</a>
-        </div>
-    </form>
+    <div class="container">
+        <form action="<?= url('registerStore'); ?>" method="post">
+            <input type="hidden" name="csrf_token" value="<?= csrf_token(); ?>">
+            <div class="mb-3">
+                <label for="reg_name" class="form-label">Name</label>
+                <input type="name" minlength="3" class="form-control" id="reg_name" aria-describedby="emailName" name="name" required>
+            </div>
+            <div class="mb-3">
+                <label for="reg_email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="reg_email" aria-describedby="emailHelp" name="email" required>
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="reg_password" class="form-label">Password</label>
+                <input type="password" minlength="6" class="form-control" id="reg_password" name="password" required>
+            </div>
+            <div class="mb-3">
+                <label for="reg_password_conf" class="form-label">Password Confirm</label>
+                <input type="password" minlength="6" class="form-control" id="reg_password_conf" name="password_conf" required>
+            </div>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary mx-1">Зарегистрироваться</button>
+                <a href="<?= url('login'); ?>" class="btn btn-danger mx-1">Назад</a>
+            </div>
+        </form>
+    </div>
 <?php
 include __DIR__ . "/../Layouts/footer.php";
 ?>
